6a7660171172708d18e53a5c50d1caad5c0298c9
Lucas Costa <francisco.lucas0507@gmail.com>
2024-01-18 20:26:36 -0300

    signin buttons

--- src/app/spotify/page.tsx
+++ src/app/spotify/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import { getServerAuthSession } from "~/server/auth";
 import { Separator } from "../_components/ui/components/ui/separator";
 import { Button } from "../_components/ui/components/ui/button";
+import { SpotifyLogginButton } from "../_components/SpotifyLoginButton";
 
 export default async function Home() {
   const session = await getServerAuthSession();
@@ -14,13 +15,13 @@ export default async function Home() {
   return (
     <div className="flex flex-1 flex-col items-center">
       <div className="flex w-full flex-1 flex-grow flex-col gap-y-4 border-muted md:max-w-2xl md:border-x-2">
-        <div className="px-8 py-4">
-          <h1>Spotify</h1>
+        <div className="flex px-8 py-8">
+          <span className="text-xl">Spotify</span>
         </div>
         <Separator />
-        <Link className="px-8  py-4" href={"/api/auth/signin"}>
-          <Button>Login</Button>
-        </Link>
+        <div className="flex w-full flex-col items-center px-8 py-8">
+          <SpotifyLogginButton />
+        </div>
       </div>
     </div>
   );
